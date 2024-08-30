c11d9cec3f8e3c394f08a37818b5c2be9407f480
Jason Showalter <jyshowalter@gmail.com>
2024-08-29 18:46:01 -0700

    updated sizing of steps

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Steps = () => {
   return (
-    <div className="flex flex-col md:flex-row items-start justify-center pb-20">
-      <div className="card bg-base-100 w-full sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mt-12">
-        <figure className="px-10 pt-20">
+    <div className="flex flex-col md:flex-row items-start justify-center pt-10 pb-32">
+      <div className="card bg-base-100 sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mx-20 mt-12">
+        <figure className="px-32 pt-20">
           <img
             src="assets\ticketpic.png"
             alt="Tickets"
             className="rounded-xl"
           />
         </figure>
-        <div className="card-body text-center py-4">
-          <h2 className="card-title text-xl font-bold justify-center">
+        <div className="card-body text-center">
+          <h2 className="card-title text-3xl font-bold justify-center">
             Tickets
           </h2>
           <p className="text-info">
@@ -22,34 +22,34 @@ const Steps = () => {
           </p>
         </div>
       </div>
-      <div className="card bg-base-100 w-full sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mx-20 mt-12">
-        <figure className="px-10 pt-20">
+      <div className="card bg-base-100 sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mx-20 mt-12">
+        <figure className="px-32 pt-20">
           <img
             src="assets\gatepic.png"
             alt="Go to gate"
             className="rounded-xl"
           />
         </figure>
-        <div className="card-body text-center py-4">
-          <h2 className="card-title text-xl font-bold justify-center">
-            Go to gate
+        <div className="card-body text-center">
+          <h2 className="card-title text-3xl font-bold justify-center">
+            Go to the gate
           </h2>
-          <p className="text-info">
+          <p className="text-info pb-12">
             After you purchase your ticket(s), head to our location at Eloise
             Asylum. You will redeem your ticket(s) at the gate.
           </p>
         </div>
       </div>
-      <div className="card bg-base-100 w-full sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mt-12">
-        <figure className="px-10 pt-20">
+      <div className="card bg-base-100 sm:w-80 md:w-96 card-bordered border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mx-20 mt-12">
+        <figure className="px-32 pt-20">
           <img
             src="assets\popcornpic.png"
             alt="Sit back and relax"
             className="rounded-xl"
           />
         </figure>
-        <div className="card-body text-center py-4">
-          <h2 className="card-title text-xl font-bold justify-center">
+        <div className="card-body text-center">
+          <h2 className="card-title text-3xl font-bold justify-center">
             Sit back and relax
           </h2>
           <p className="text-info">
